ba9c85612fc62fb51099102e89e8fa70fd646e19
Pantersxx3 <horacio.fanelli@gmail.com>
2024-04-16 20:51:38 -0300

    Add files via upload

---
+++ progressbar.py
@@ -0,0 +1,38 @@
+import time
+
+def progressBar(iterable, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iterable    - Required  : iterable object (Iterable)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    total = len(iterable)
+    # Progress Bar Printing Function
+    def printProgressBar (iteration):
+        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+        filledLength = int(length * iteration // total)
+        bar = fill * filledLength + '-' * (length - filledLength)
+        print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Initial Call
+    printProgressBar(0)
+    # Update Progress Bar
+    for i, item in enumerate(iterable):
+        yield item
+        printProgressBar(i + 1)
+    # Print New Line on Complete
+    print()
+    
+def set_progress_bar(message, l, whait) :       
+    # A List of Items
+    items = list(range(0, 57))
+
+    # A Nicer, Single-Call Usage
+    for item in progressBar(items, prefix = message, suffix = '', length = l):
+        # Do stuff...
+        time.sleep(whait/l)
